6ef901a0c0616ee199f5ca24f0cfefddb4986323
auwal <a4auwal@gmail.com>
2022-09-22 22:30:03 +0100

    enable data binding

--- app/src/main/java/com/a4gumel/taska/activities/MainActivity.kt
+++ app/src/main/java/com/a4gumel/taska/activities/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.a4gumel.taska.R
+import com.a4gumel.taska.databinding.ActivityMainBinding
+import com.a4gumel.taska.viewModel.NoteActivityViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteActivityViewModel: NoteActivityViewModel;
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         installSplashScreen()
