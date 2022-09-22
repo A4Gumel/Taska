fc3ebba5a285c5491a5c7b9b9bc3adea848b2486
auwal <a4auwal@gmail.com>
2022-09-22 23:41:21 +0100

    HomeFragment.kt data binding

--- app/src/main/java/com/a4gumel/taska/activities/MainActivity.kt
+++ app/src/main/java/com/a4gumel/taska/activities/MainActivity.kt
@@ -2,14 +2,21 @@ package com.a4gumel.taska.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModelProvider
 import com.a4gumel.taska.R
 import com.a4gumel.taska.databinding.ActivityMainBinding
+import com.a4gumel.taska.db.NoteDatabase
+import com.a4gumel.taska.repository.NoteRepository
 import com.a4gumel.taska.viewModel.NoteActivityViewModel
+import com.a4gumel.taska.viewModel.NoteActivityViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var noteActivityViewModel: NoteActivityViewModel;
+    private val TAG: String = "MainActivity"
+
+    private lateinit var noteActivityViewModel: NoteActivityViewModel;
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +24,18 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        try {
+            setContentView(binding.root)
+            val noteRepository = NoteRepository(NoteDatabase(this))
+            val noteActivityViewModelFactory = NoteActivityViewModelFactory(noteRepository)
+            noteActivityViewModel = ViewModelProvider(this,
+                noteActivityViewModelFactory)[NoteActivityViewModel::class.java]
+        } catch (e: Exception) {
+
+            Log.e(TAG, e.toString())
+        }
     }
 }
